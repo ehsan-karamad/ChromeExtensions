877ed8beca92f2aeed9663266d58b84fbbc8487c
Ehsan Karamad <ekaramad@chromium.org>
2015-12-11 16:49:54 -0500

    Storing some last minute changes.

--- ImageDownloader/messages.js
+++ ImageDownloader/messages.js
@@ -1,4 +1,8 @@
-var MSG_PARSE_DOCUMENT = 'parse-document';
-var MSG_PARSING_COMPLETE = 'parse-complete';
-var MSG_SEND_IMAGE_URLS = 'send-image-url';
-var MSG_IMAGE_URLS = 'image-urls';
+// Extension Messages to Content Scripts
+var index = 0;
+var MSG_PARSE_DOCUMENT = index++;
+var MSG_SEND_IMAGE_LIST = index++;
+
+// Content Script to Extension
+var MSG_DOCUMENT_PARSED = index++;
+var MSG_IMAGE_LIST = index++;

e9d5d8313a700be9c7e0142a8431947ce3c92a95
ekaramad <ekaramad@ekaramad-macbookair.roam.corp.google.com>
2015-12-15 00:10:47 -0500

    Added a sample image tagging mechanism.

--- ImageDownloader/background.js
+++ ImageDownloader/background.js
@@ -8,13 +8,54 @@ function sendParsePageRequest(finishedCallback) {
   })
 }
 
+function randomInt(l, r) {
+  return Math.floor(Math.random() * (r - l) + l);
+}
 
+function randomDate() {
+  var now = new Date(Date.now());
+  var fullYear = now.getFullYear();
+  var yearBefore = randomInt(0, 10);
+  var month = randomInt(0, 12) + 1;
+  if (yearBefore === 0) {
+    month = now.getMonth() - randomInt(0, now.getMonth());
+  }
+  var day = randomInt(0, 28) + 1;
+  if (month === now.getMonth() && yearBefore === 0) {
+    day = now.getDate() - randomInt(0, now.getDate());
+  }
+  var d = new Date(fullYear - yearBefore, month, day);
+  return d.toDateString();
+}
 
-chrome.runtime.onMessage.addListener(function(msg) {
+function ImageRequestJob(image, tabId) {
+  this.image = image;
+  this.tabId = tabId;
+}
 
+ImageRequestJob.prototype.run = function() {
+  // simply call back and tell the tab that the job completed.
+  var args = {
+    "image": this.image,
+    "result": {
+      "date": randomDate()
+    }
+  };
+  chrome.tabs.sendMessage(this.tabId, new Message(MSG_INFO_READY, args));
+};
+
+chrome.runtime.onMessage.addListener(function(msg, sender) {
+  if (msg.header === MSG_IMAGE_LIST) {
+    var list = msg.args;
+    for (var i = 0; i < list.length; ++i) {
+      var image = list[i];
+      var job = new ImageRequestJob(image, sender.tab.id);
+      job.run();
+    }
+  }
 });
 
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  tab.sendMessage
+  chrome.tabs.sendMessage(tab.id, new Message(MSG_SEND_IMAGE_LIST, null));
 });
--- ImageDownloader/messages.js
+++
@@ -1,8 +0,0 @@
-// Extension Messages to Content Scripts
-var index = 0;
-var MSG_PARSE_DOCUMENT = index++;
-var MSG_SEND_IMAGE_LIST = index++;
-
-// Content Script to Extension
-var MSG_DOCUMENT_PARSED = index++;
-var MSG_IMAGE_LIST = index++;
